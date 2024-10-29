20a2982c15338dc0b21a240db7eca52b6f432232
okadim <okadim@unifonic.com>
2024-10-30 00:31:45 +0400

    removed api key

--- manatal_service/app/config.py
+++ manatal_service/app/config.py
@@ -6,5 +6,6 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Access environment variables
+# Access environment variables:
+# this one is old because i was using pinecone before faiss, kept it in case i want to use it again
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
